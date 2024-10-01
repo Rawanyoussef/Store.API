36303d4db5767f5d99c1539a6a6500070224ea60
rawanyoussef <rawanyoussef@LAPTOP-3V5FQ72L>
2024-10-02 01:47:45 +0300

    Add project files.

---
+++ Store.Web/Extentions/ApplicationServiceExetension.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Repository.Interfaces;
+using Store.Repository.Repositories;
+using Store.Service.Services.ProductServices.Dtos;
+using Store.Service.Services.ProductServices;
+using Store.Service.Services;
+using Store.Service.HandelResponse;
+
+namespace Store.Web.Extentions
+{
+    public static class ApplicationServiceExetension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddAutoMapper(typeof(ProductProfile));
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState.
+                                Where(model => model.Value?.Errors.Count > 0)
+                                .SelectMany(model => model.Value?.Errors)
+                                .Select(error => error.ErrorMessage).ToList();
+
+
+                    var errorresponse = new ValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+                    return new BadRequestObjectResult(errorresponse);
+                };
+            });
+            return services;
+
+
+        }
+    }
+}
---
+++ Store.Web/Program.cs
@@ -0,0 +1,58 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Store.Data.Contexts;
+using Store.Repository;
+using Store.Repository.Interfaces;
+using Store.Repository.Repositories;
+using Store.Service.HandelResponse;
+using Store.Service.Services;
+using Store.Service.Services.ProductServices;
+using Store.Service.Services.ProductServices.Dtos;
+using Store.Web.Extentions;
+using Store.Web.Helper;
+using Store.Web.MiddleWare;
+
+namespace Store.Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<StoreDbContext>( options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            }
+                );
+            builder.Services.AddApplicationServices();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+           await ApplySeeding.ApplySeedingAsync( app );
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseMiddleware<ExpetionMiddleWare>();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles(); 
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
